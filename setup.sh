c9f3e4e1dec9e73632c6045f432f0e1410dcd804
umesh_new <umesh_new@gmail.com>
2025-06-03 22:34:08 +0100

    PROJ-123: Add login feature

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-echo "bhai"
-git config core.hooksPath
-chmod +x .githooks/commit-msg 
+#!/bin/bash
+git config core.hooksPath .githooks
+chmod +x .githooks/commit-msg
+echo "Git hooks path configured and hook made executable."
+
